1791b0346f3b58db7b4bd79af7ffcd7b856b7948
briansiervi <brianmazini@gmail.com>
2024-09-14 10:27:25 -0300

    integração com as apis finalizada

---
+++ web/src/components/pending-goals.tsx
@@ -0,0 +1,31 @@
+import { Plus } from 'lucide-react'
+import { OutlineButton } from './ui/outline-button'
+import { getPendingGoals } from '../http/get-pending-goals'
+import { useQuery } from '@tanstack/react-query'
+
+export function PendingGoals() {
+  const { data } = useQuery({
+    queryKey: ['pending-goals'],
+    queryFn: getPendingGoals,
+  })
+
+  if (!data) {
+    return null
+  }
+
+  return (
+    <div className="flex flex-wrap gap-3">
+      {data.map(goal => {
+        return (
+          <OutlineButton
+            key={goal.id}
+            disabled={goal.completionCount >= goal.desiredWeeklyFrequency}
+          >
+            <Plus className="size-4 text-zinc-600" />
+            {goal.title}
+          </OutlineButton>
+        )
+      })}
+    </div>
+  )
+}
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./app";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { App } from './app'
+import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-createRoot(document.getElementById("root")!).render(
+const queryClient = new QueryClient()
+
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
-);
+)
